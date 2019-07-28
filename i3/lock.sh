eef6d98ab6e7d1158c9ba8ce6e7598155152ebea
Romulo Lobosco <rlobosco_prest@telecine.com.br>
2019-07-28 17:53:56 -0300

    change lockscreen script

--- i3/lock.sh
+++ i3/lock.sh
@@ -2,7 +2,7 @@
 
 TMPBG=lockscreen.png
 scrot $TMPBG 
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG -blur 0x5 $TMPBG
 convert $TMPBG -gravity center -composite -matte $TMPBG
 #i3lock -u -i $TMPBG
 i3lock -i $TMPBG
